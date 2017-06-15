25c9bafdf9f8d32e8668797d7d6d5a88e16b9144
nectar <nectar@Nectar-PC>
2017-06-14 22:06:48 -0300

    voltando ao que estava funcinando

--- iot.placaExpansao.programacao.ino
+++ iot.placaExpansao.programacao.ino
@@ -10,10 +10,9 @@
 #include "mcp_can.h"
 #include <Wire.h>
 #include "Adafruit_MCP23017.h"
-//#include "ctmNectar.h"
-#include "definicoes.h"
+#include "ctmNectar.h"
 
-#define DEBUG
+//#define DEBUG
 #define CENTRAL_ID 0x00
 
 const int SPI_CS_PIN = 10;
@@ -92,12 +91,11 @@ void setup()
   Serial.println(F("Remota: CAN BUS init ok!"));
 #endif
 
-  unsigned char msgCfg[3] = {especial, 1, 1};
+  unsigned char msgCfg[1] = {ONLINE};
   CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgCfg), msgCfg);
 
   //Inicializa o Watchdog
   wdt_enable(WDTO_250MS);
-  pinMode(8, OUTPUT);
 }
 
 void loop() {
@@ -120,7 +118,6 @@ void loop() {
     }
   }
   wdt_reset();  //  reseta o watchdog
-  digitalWrite(8, !digitalRead(8));
 }
 
 void iniciarIO() {
@@ -164,10 +161,10 @@ void lerEntradas() {
 */
 void atualizarSaidas(unsigned char saida, unsigned char estado) {
 #if defined(DEBUG)
-  Serial.print("Saida ");
-  Serial.print(saida);
-  Serial.print(": ");
-  Serial.println(estado);
+Serial.print("Saida ");
+Serial.print(saida);
+Serial.print(": ");
+Serial.println(estado);
 
 #endif
   if (saida < 8) {
@@ -201,8 +198,7 @@ void salvarSensorConfig() {
 /**
    Envia configurações para a central
 */
-/*
-  void enviarConfig() {
+void enviarConfig() {
   unsigned char msgCfg[2] = {0};
 
   msgCfg[0] = SEND_TIME;
@@ -210,66 +206,33 @@ void salvarSensorConfig() {
 
   CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgCfg), msgCfg);
 
-  }
-*/
+}
+
 /**
    Envia dados para a central
 */
-//void enviarDados() {
-//  unsigned char msgDados[2];
-//
-//  //entradas
-//  msgDados[0] = INPUT_1_STATE;
-//  msgDados[1] = 0;
-//  //Converte os dados binarios em um unico numero de 8bits
-//  for (char i = 7; i >= 0; i--) {
-//    bitWrite(msgDados[1], i, entradas[i]);
-//  }
-//  CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgDados), msgDados);
-//  delay(10);
-//
-//  //saidas
-//  msgDados[0] = OUTPUT_1_STATE;
-//  msgDados[1] = 0;
-//  //Converte os dados binarios em um unico numero de 8bits
-//  for (char i = 7; i >= 0; i--) {
-//    bitWrite(msgDados[1], i, saidas[i]);
-//  }
-//  CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgDados), msgDados);
-//  msUltimoEnvio = millis();
-//}
-
-void lerEntradaDigital(int8_t entrada) {
-  if (entrada >= 0 && entrada < 8) {
-    unsigned char msg[4] = {0};
-
-    msg[0] = (uint8_t)entradaDigital;
-    msg[1] = (uint8_t)entrada;
-    msg[2] = entradas[entrada];
-    CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msg), msg);
-  }
-}
-
 void enviarDados() {
-  unsigned char msgDados[3];
-  msgDados[0] = (uint8_t)entradaDigital;
+  unsigned char msgDados[2];
 
-  for (uint8_t i = 0; i < 7; i++) {
-    msgDados[1] = i;
-    msgDados[2] = (uint8_t)entradas[i];
-    CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgDados), msgDados);
-    delay(10);
+  //entradas
+  msgDados[0] = INPUT_1_STATE;
+  msgDados[1] = 0;
+  //Converte os dados binarios em um unico numero de 8bits
+  for (char i = 7; i >= 0; i--) {
+    bitWrite(msgDados[1], i, entradas[i]);
   }
+  CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgDados), msgDados);
+  delay(10);
 
-  msgDados[0] = (uint8_t)saidaDigital;
-
-  for (uint8_t i = 0; i < 7; i++) {
-    msgDados[1] = i;
-    msgDados[2] = (uint8_t)saidas[i];
-    CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgDados), msgDados);
-    delay(10);
+  //saidas
+  msgDados[0] = OUTPUT_1_STATE;
+  msgDados[1] = 0;
+  //Converte os dados binarios em um unico numero de 8bits
+  for (char i = 7; i >= 0; i--) {
+    bitWrite(msgDados[1], i, saidas[i]);
   }
-
+  CAN.sendMsgBuf(sensorConfig.endereco, 0, sizeof(msgDados), msgDados);
+  msUltimoEnvio = millis();
 }
 
 bool pacoteRecebido() {
@@ -283,10 +246,9 @@ bool pacoteRecebido() {
 
   //Estrutura que vai receber a mensagem
   struct {
-    uint8_t idRede;
-    uint8_t tipoGrandeza;
-    uint8_t grandeza;
-    int16_t valor;
+    unsigned int canId;
+    unsigned char comando;
+    unsigned char msg[6];
   } canPkt;
 
   //Le a mensagem da placa
@@ -300,15 +262,14 @@ bool pacoteRecebido() {
     return false;
   }
 
-  //Pega o ID do destino
-  canPkt.idRede = buf[0];
-
+  //Pega o ID do receptor
+  canPkt.canId = buf[0];
   //confere se o ID é para este dispositivo
-  if (canPkt.idRede != sensorConfig.endereco) {
+  if (canPkt.canId != sensorConfig.endereco) {
     leSensorConfig();
 #if defined(DEBUG)
     Serial.print(F("Esta mensagem nao e para este dispositivo: "));
-    Serial.print(canPkt.idRede);
+    Serial.print(canPkt.canId);
     Serial.print(F(" != "));
     Serial.println(sensorConfig.endereco);
 #endif
@@ -316,64 +277,30 @@ bool pacoteRecebido() {
   }
 
   //Pega o comando enviado
-  canPkt.tipoGrandeza = buf[1];
-  canPkt.grandeza = buf[2];
-  canPkt.valor = word(buf[3], buf[4]);
+  canPkt.comando = buf[1];
 
-  //verifica o tipo de grandeza
-  switch (canPkt.tipoGrandeza) {
-    /*--------entradas digitais-----------*/
-    case entradaDigital: {
-#if defined(DEBUG)
-        Serial.println(F("entradaDigital"));
-#endif
-        lerEntradaDigital(canPkt.grandeza);
-        break;
-      };
-    /*--------fim das entradas digitais-----------*/
-
-    /*--------saidas digitais-----------*/
-    case saidaDigital: {
-        canPkt.valor = canPkt.valor / 100;
-#if defined(DEBUG)
-        Serial.println(F("saidaDigital"));
-        Serial.println(canPkt.valor);
-#endif
-        atualizarSaidas(canPkt.grandeza, canPkt.valor);
-        break;
-      };
-      /*--------fim das saidas digitais-----------*/
-  };
-  return true;
-}
-
-
-
-
-
-/*
   //verifica o comando
-  switch (canPkt.tipoGrandeza) {
+  switch (canPkt.comando) {
     case SEND_CONFIG: {
-  #if defined(DEBUG)
+#if defined(DEBUG)
         Serial.println(F("SEND_CONFIG"));
-  #endif
+#endif
         enviarConfig();
       };
       break;
 
     case SEND_DATA: {
-  #if defined(DEBUG)
+#if defined(DEBUG)
         Serial.println(F("SEND_DATA"));
-  #endif
+#endif
         enviarDados();
       };
       break;
 
     case CHANGE_ID: {
-  #if defined(DEBUG)
+#if defined(DEBUG)
         Serial.println(F("CHANGE_ID"));
-  #endif
+#endif
         if (buf[2] > 0) sensorConfig.endereco = buf[2];
         //salva novas configuracoes
         salvarSensorConfig();
@@ -382,9 +309,9 @@ bool pacoteRecebido() {
       break;
 
     case CHANGE_SEND_TIME: {
-  #if defined(DEBUG)
+#if defined(DEBUG)
         Serial.println(F("CHANGE_SEND_TIME"));
-  #endif
+#endif
         if (buf[2] >= 0) sensorConfig.intervaloEnvio = buf[2];
         //salva novas configuracoes
         salvarSensorConfig();
@@ -393,18 +320,17 @@ bool pacoteRecebido() {
       break;
 
     case CHANGE_OUTPUT_STATE: {
-  #if defined(DEBUG)
+#if defined(DEBUG)
         Serial.println(F("CHANGE_OUTPUT_STATE"));
-  #endif
+#endif
         atualizarSaidas(buf[2], buf[3]);
       };
       break;
 
-  #if defined(DEBUG)
+#if defined(DEBUG)
     default:
       Serial.println(F("Comando nao reconhecido"));
-  #endif
+#endif
   }
   return true;
-  }
-*/
+}
